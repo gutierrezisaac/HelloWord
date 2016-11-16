aae423bdae8a924f37a0756436d2ad1fa8e01657
Matthew Bowden <bowdenm@spu.edu>
2016-11-16 15:31:16 -0800

    Added new stuff to Hello World

--- HelloWorldGit/HelloWorldGit/HelloWorld.cpp
+++ HelloWorldGit/HelloWorldGit/HelloWorld.cpp
@@ -4,8 +4,8 @@ using namespace std;
 int main()
 {
 	cout << "Hello World" << endl; 
-	cout << "Hello second world" << endl; 
-	cout << "Hey matt" << endl; 
+	cout << "Hello second world -- the end" << endl; 
 
+	cout << "Hey Isaac!" << endl;
 	return 0;
 }
